0f12f6212fbeaf79b54fc0f3c7ab035a395aa05b
Mr.Kelvin <locnguyenphuocloc67@gmail.com>
2022-02-22 21:19:21 +0700

    devA_dev

--- method/featureA.js
+++ method/featureA.js
@@ -1,15 +1,13 @@
 function featureA() {
   console.log("dev A code");
   console.log("dev A tự fix bug");
-    console.log("dev A code");
-    console.log("dev B fix bug");
+  console.log("dev A code");
+  console.log("dev B fix bug");
 }
-<<<<<<< HEAD
 
-function feature2A () {
-  console.log('hello, ahihi')
-=======
-function testA(){
-  console.log('test A')
->>>>>>> master
+function feature2A() {
+  console.log("hello, ahihi");
+}
+function testA() {
+  console.log("test A");
 }
